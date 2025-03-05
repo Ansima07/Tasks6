52003ba464ab836392f86bfce9be3f9b550dd01d
HP CA <murayansima@gmail.com>
2025-03-05 19:31:39 +0100

    Commit #2
    In OV, on line i changed the name ampire to benjamin in  NV.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         public static void main(String[] args) {
             managment manager1= new managment("Marie Muray", "murayansima@mail.com", 726974611 );
             manager1.DisplayManagmentInfo();
-            managment2 manager2= new managment2("Ampire Muray", "murayampire@gmail.com", 726335511);
+            managment2 manager2= new managment2("Benjamin Muray", "murayampire@gmail.com", 726335511);
             manager2.DisplayManagmentInfo();
         }
 }
